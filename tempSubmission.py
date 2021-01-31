a364eaa40587c249f2dd269981e5166e188d9672
Darren Chan <cthdarren88@gmail.com>
2021-01-31 16:53:09 +0800

    reworked flow of variables and generation of files

--- tempSubmission.py
+++ tempSubmission.py
@@ -1,40 +1,65 @@
-import random
-import requests
+import random, os, requests, sys
 from datetime import datetime
-
-groupCode = ""
-memberId = ""
-pin = ""
-date = datetime.now().strftime("%d/%m/%Y")
-if int(datetime.now().strftime("%H")) < 12:
-    meridies = "AM"
-else:
-    meridies = "PM"
-
-if meridies == "AM":
-    temperature = random.uniform(35.4, 36.7)
-if meridies == "PM":
-    temperature = random.uniform(35.9, 36.9)
-
-temperature = round(temperature,1)
-
-data = {
-    "groupCode":groupCode,
-    "date": date,
-    "meridies": meridies,
-    "memberId": memberId,
-    "temperature": temperature,
-    "pin": pin
-}
-
 try:
-    result = requests.post("https://temptaking.ado.sg/group/MemberSubmitTemperature", data = data)
-    file = open("tempLog.txt", "a")
-    file.write(datetime.now().strftime("%d%m%y-%H%M") + "|" + str(temperature) + "|" + meridies + "\n")
-except Exception as a:
-    file.write(datetime.now().strftime("%d%m%y-%H%M") + "|" + str(a))
+    import uservars
+except ModuleNotFoundError:
+    if not os.path.exists("uservars.py"):
+        uservarFile = open("uservars.py", "a")
+        uservarFile.writelines([
+            'groupCode = ""',
+            'memberId = ""',
+            'pin = ""'
+        ])
+    sys.exit("uservars file has been generated, please fill in the relevant details in the file")
 
-file.close()
+def getDate():
+    return datetime.now().strftime("%d/%m/%Y")
 
-if result.text == "OK":
-    print("Success! Temperature recorded as: {}°C at {}, {} time".format(temperature, date, meridies) )
+def getMeridies():
+    if int(datetime.now().strftime("%H")) < 12:
+        return "AM"
+    else:
+        return "PM"
+
+def generateTemp():
+    if getMeridies() == "AM":
+        temperature = random.uniform(35.4, 36.7)
+    if getMeridies() == "PM":
+        temperature = random.uniform(35.9, 36.9)
+
+    return round(temperature,1)
+
+def sendReq():
+    temperature = generateTemp()
+    date = getDate()
+    meridies =  getMeridies()
+
+    data = {
+        "groupCode":uservars.groupCode,
+        "date": date,
+        "meridies": meridies,
+        "memberId": uservars.memberId,
+        "temperature": temperature,
+        "pin": uservars.pin
+    }
+    try:
+        result = requests.post("https://temptaking.ado.sg/group/MemberSubmitTemperature", data = data)
+        file = open("tempLog.txt", "a")
+        file.write(datetime.now().strftime("%d%m%y-%H%M") + "|" + str(temperature) + "|" + meridies + "\n")
+        if result.text == "OK":
+            print("Success! Temperature recorded as: {}°C at {}, {} time".format(temperature, date, meridies) )
+            
+    except Exception as a:
+        file.write(datetime.now().strftime("%d%m%y-%H%M") + "|" + str(a) + "\n")
+        
+    file.close()
+
+def checkVariables():
+    if uservars.groupCode == "" or uservars.memberId == "" or uservars.pin == "":
+        print("Please fill in all the fields in uservars.py before continuing")
+        return False
+
+    return True
+    
+if checkVariables():
+    sendReq()
